73da01e5c15f3d7002e1b4037ac7c082589b6ccb
qiqunnie@mozi.top <qiqunnie@mozi.top>
2019-05-17 18:38:20 +0800

    init

---
+++ src/main/java/com/test/TestAction.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * Date: 2019/5/13 11:31
+ * Description:
+ *
+ * @author nieqq
+ */
+public class TestAction {
+    public static int add(int i) {
+        return i + 100;
+    }
+}
